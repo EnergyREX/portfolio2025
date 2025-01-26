a4ac072ba46f1430fd48da4f7f9745932744b65f
EnergyREX <energy.rex.2003@gmail.com>
2025-01-26 19:31:54 +0100

    upd: consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-const HERO_MESSAGES = {
+const HERO_MESSAGES: any = {
   es: {
     title: "Hey, Soy Fran.",
     subtitle: "Un desarrollador de Almería. Me encanta desarrollar soluciones en la web. He trabajado con tecnologías como React o Astro.",
@@ -13,5 +13,36 @@ const HERO_MESSAGES = {
   }
 
 }
+
+const PROJECTS_CARDS: [Object, Object] = [
+  {
+    id: 1,
+    en: {    
+      title: "Ollama-Chat",
+      shortDesc: "An applicacion who connects and stores in a Database your conversations with the Ollama Chatbot.",
+      },
+    es: {
+      title: "Ollama-Chat",
+      shortDesc: "Una aplicación que conecta y guarda en una base de datos tus conversaciones con la IA Ollama.",
+    },
+    link: "/ollama-chat",
+    repo: "https://github.com/EnergyREX/ollama-chat",
+    bgImg: ""
+  },
+  {
+    id: 1,
+    en: {    
+      title: "DAW's Final Degree Project - MyClinic",
+      shortDesc: "An application to manage a clinic appointments, medical records, users... of a clinic. Developed with Laravel.",
+      },
+    es: {
+      title: "Trabajo de Final de Grado - MyClinic",
+      shortDesc: "Una aplicación para administrar citas, registros médicos, usuarios... de una clínica. Desarrollado con Laravel.",
+    },
+    link: "/myclinic",
+    repo: "https://github.com/EnergyREX/ollama-chat",
+    bgImg: ""
+  }
+] 
 
 export {HERO_MESSAGES}
