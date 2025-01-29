b275429c63c136179ef927a7ac0e50ab6ae3cd00
EnergyREX <energy.rex.2003@gmail.com>
2025-01-29 14:45:55 +0100

    add: more content

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,17 @@
 // For status (if available or not)
 const STATUS = true;
 
+const ABOUT: any = {
+  en: {
+    title: "I'm Francisco",
+    text: "When I was 18, I had discovered my passion on programming, thanks to a friend who gifted me a Java course. After completing Bachillerato, I had decided to go to a T&D degree and learn about web development."
+  },
+  es: {
+    title: "Soy Francisco",
+    text: "Cuando tenía 18 años, descubrí mi amor por la programación, gracias a un amigo que me regaló un curso de Java. Después de terminar Bachiller, decidí hacer un grado de Formación profesional y ahí, aprender sobre el desarrollo web."
+  }
+}
+
 // Messages for Hero Section, in Spanish and English
 const HERO_MESSAGES: any = {
   es: {
@@ -74,4 +85,4 @@ const PROJECTS: Array<{}> = [
   }
 ] 
 
-export {HERO_MESSAGES, STATUS, PROJECTS}
+export {ABOUT, HERO_MESSAGES, STATUS, PROJECTS}
