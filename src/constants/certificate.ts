ed4b32f6896d32e68452822fa24dbd0f3539e296
EnergyREX <energy.rex.2003@gmail.com>
2025-02-11 11:21:41 +0100

    feat: Certificate section

---
+++ src/constants/certificate.ts
@@ -0,0 +1,46 @@
+const CERTIFICATE_TITLE: any = {
+  en: {
+    title: "Certifications and Degrees",
+  },
+  es: {
+    title: "Certificaciones y grados",
+  }
+}
+
+const CERTIFICATES: any = [{
+  id: 1,
+  issuer: "OpenWebinars",
+  certificate_url: "https://openwebinars.net/cert/VhOQ",
+  en: {
+    title: "Website development with Astro",
+    details: "A course completed in January 2025. This course taught me about the basics in Astro.build. This website was developed with Astro.",
+    date: "January 2025",
+    btn: "Certificate"
+  },
+  es: {
+    title: "Creación de páginas web con Astro",
+    details: "Un curso completado en Enero de 2025. Con este curso, aprendí a hacer páginas con Astro.build. Este sitio web es un ejemplo de ello.",
+    date: "Enero 2025",
+    btn: "Certificado"
+  }
+},
+{
+  id: 2,
+  issuer: "OpenWebinars",
+  certificate_url: "https://openwebinars.net/cert/anRf",
+  en: {
+    title: "Figma Course",
+    details: "A course completed in January 2025. With this course, I've learnt how to develop wireframes and prototypes with Figma.",
+    date: "January 2025",
+    btn: "Certificate"
+  },
+  es: {
+    title: "Curso de Figma",
+    details: "Un curso completado en Enero de 2025. Con este curso, aprendí a hacer wireframes y prototipos usando Figma.",
+    date: "Enero 2025",
+    btn: "Certificado"
+  }
+}
+]
+
+export {CERTIFICATES, CERTIFICATE_TITLE}
